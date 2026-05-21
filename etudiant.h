689979836ff0f9c846a707cf9e30af0f69af3a25
isaac-O95 <neiityetheone@gmail.com>
2026-05-21 17:21:22 +0200

    Update etudiant.h

--- etudiant.h
+++ etudiant.h
@@ -1,1 +1,12 @@
+#ifndef ELEVE_H
+#define ELEVE_H
 
+#include "qcm.h"
+
+void menuEleve(void);
+void choisirQCM(void);
+void passerQCM(QCM qcm);
+void poserQuestion(QCM qcm, int indiceQuestion, int reponsesEleve[MAX_QUESTIONS][MAX_REPONSES]);
+void afficherResultat(float note);
+
+#endif
