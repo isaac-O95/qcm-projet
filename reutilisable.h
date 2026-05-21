2b33e5dbebaf54da1130957f2d549b1653b14804
isaac-O95 <neiityetheone@gmail.com>
2026-05-21 17:19:45 +0200

    Update reutilisable.h

--- reutilisable.h
+++ reutilisable.h
@@ -1,1 +1,12 @@
+#ifndef REUTILISABLE_H
+#define REUTILISABLE_H
 
+void viderBuffer(void);
+int lireEntier(void);
+void lireTexte(char texte[], int taille);
+int demanderOuiNon(char message[]);
+void pauseProgramme(void);
+void nettoyerEcran(void);
+int demanderChoixMenu(int min, int max);
+
+#endif
