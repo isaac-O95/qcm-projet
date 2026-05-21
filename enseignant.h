201adc903c7e3e4ccb74803f25a0cc3296d3cfed
isaac-O95 <neiityetheone@gmail.com>
2026-05-21 17:22:17 +0200

    Update enseignant.h

--- enseignant.h
+++ enseignant.h
@@ -1,1 +1,15 @@
+#ifndef ENSEIGNANT_H
+#define ENSEIGNANT_H
 
+#include "qcm.h"
+
+int verifierMotDePasse(void);
+void menuEnseignant(void);
+void creerQCM(void);
+void saisirParametresQCM(QCM *qcm);
+void ajouterQuestion(QCM *qcm);
+void modifierQCM(void);
+void supprimerQCM(void);
+void afficherListeQCMEnseignant(void);
+
+#endif
