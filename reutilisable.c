4fd1d251c79571b0e38a30d347232d53ff736135
isaac-O95 <neiityetheone@gmail.com>
2026-05-24 12:56:59 +0200

    Update reutilisable.c

--- reutilisable.c
+++ reutilisable.c
@@ -1,29 +1,35 @@
 #include <stdio.h>
 #include "reutilisable.h"
 
-void ViderBuffer(void){
-  int c;
-do {
-    c = getchar();
-} while(c != '\n');
+void ViderBuffer(void) {
+    int c;
+
+    do {
+        c = getchar();
+    } while (c != '\n');
 }
 
-int lireEntier(void){
+int lireEntier(void) {
     int nombre;
-    scanf('%d', &nombre);
+
+    scanf("%d", &nombre);   // %d doit être entre guillemets doubles
     ViderBuffer();
- return nombre;
+
+    return nombre;
 }
 
-void lireTexte(char texte[], int taille){
-  fgets(texte, taille, stdin);
-      int i=0;
-      while(texte[i] != '\0'){
-        if(texte[i] == '\n'){
-          texte[i] = '\0';
+void lireTexte(char texte[], int taille) {
+    int i = 0;
+
+    fgets(texte, taille, stdin);
+
+    // On enlève le retour à la ligne ajouté par fgets
+    while (texte[i] != '\0') {
+        if (texte[i] == '\n') {
+            texte[i] = '\0';
         }
         i++;
-      }
+    }
 }
 
 int demanderOuiNon(char message[]) {
@@ -31,6 +37,7 @@ int demanderOuiNon(char message[]) {
 
     choix = -1;
 
+    // On attend seulement 0 ou 1
     while (choix != 0 && choix != 1) {
         printf("%s (1 = oui, 0 = non) : ", message);
         choix = lireEntier();
@@ -39,12 +46,14 @@ int demanderOuiNon(char message[]) {
             printf("Reponse invalide.\n");
         }
     }
+
     return choix;
 }
 
 void nettoyerEcran(void) {
     int i;
 
+    // On saute plusieurs lignes pour séparer les menus
     for (i = 0; i < 30; i++) {
         printf("\n");
     }
@@ -55,6 +64,7 @@ int demanderChoixMenu(int min, int max) {
 
     choix = min - 1;
 
+    // On redemande tant que le choix n'est pas dans l'intervalle
     while (choix < min || choix > max) {
         printf("Votre choix : ");
         choix = lireEntier();
