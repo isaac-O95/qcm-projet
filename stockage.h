45a8729dbc1569162c8bb15a2bc26e51b7f264fd
isaac-O95 <neiityetheone@gmail.com>
2026-05-24 12:19:07 +0200

    Update stockage.h

--- stockage.h
+++ stockage.h
@@ -1,13 +1,13 @@
 #ifndef STOCKAGE_H
 #define STOCKAGE_H
-
 #include "qcm.h"
 
-int sauvegarderQCM(QCM qcm);
-int chargerQCM(char nomFichier[], QCM *qcm);
-void ajouterQCMDansListe(char nomQCM[]);
-void afficherListeQCM(void);
-int compterQCMDisponibles(void);
-int recupererNomQCM(int numero, char nomQCM[]);
+void nettoyerLigne(char texte[]);
+int enregistrerQCM(QCM qcm);
+int lireQCM(char nomFichier[], QCM *qcm);
+void ajouterDansListe(char nomQCM[]);
+void voirListeQCM(void);
+int nbQCM(void);
+int chercherNomQCM(int numero, char nomQCM[]);
 
 #endif
